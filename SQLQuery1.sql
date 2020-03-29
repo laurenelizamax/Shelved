d00fbd7f4da5d613941c6bb870e42cc3f1f78948
Lauren <laurenelizamax@icloud.com>
2020-03-29 10:09:37 -0500

    changed up navbar

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿---- Trip Inserts
+--INSERT INTO Trip (Title, StartDate, EndDate, Notes, IsFavorite, ApplicationUserId)
+--VALUES ('Toronto 2013', '9-3-13','9-10-13','Went to visit Erika','true', '00000000-ffff-ffff-ffff-ffffffffffff');
+
+---- City Inserts
+--INSERT INTO City (CityName, [Description], ApplicationUserId)
+--VALUES ('Montreal', 'Canada', '00000000-ffff-ffff-ffff-ffffffffffff');
+
+---- TripCity Inserts
+--INSERT INTO TripCity (TripId, CityId)
+--VALUES (2,3)
+
